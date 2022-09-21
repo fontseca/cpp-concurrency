c0489a9194188717e804375f59246b177102aff2
Jeremy Fonseca <alx.17@outlook.com>
2022-09-21 12:34:52 -0600

    refactor: change some names

--- TooManyTasksNotEnoughSpeed/Main.cpp
+++ TooManyTasksNotEnoughSpeed/Main.cpp
@@ -11,20 +11,20 @@
 
 #include "Includes/EmailQueueManager.hpp"
 
-auto start_time = std::chrono::high_resolution_clock::now();
+const auto START_TIME = std::chrono::high_resolution_clock::now();
 
-void handle_end(int signum)
+void handle_process_finalization(int signum)
 {
   if (signum == SIGINT)
   {
     auto end_time = std::chrono::high_resolution_clock::now();
-    auto elapsed_time = std::chrono::duration_cast<std::chrono::seconds>(end_time - start_time);
+    auto elapsed_time = std::chrono::duration_cast<std::chrono::seconds>(end_time - START_TIME);
     std::cout << "\ntotal time to process emails: " << elapsed_time.count() << "s" << std::endl;
     exit(signum);
   }
 };
 
-void process_queue(const std::string_view recipient_name, const std::string_view recipient_email)
+void process_single_queue(const std::string_view recipient_name, const std::string_view recipient_email)
 {
   EmailQueueManager email_que_managuer{};
   email_que_managuer.QueueProcessor(recipient_name, recipient_email);
@@ -32,11 +32,11 @@ void process_queue(const std::string_view recipient_name, const std::string_view
 
 auto main() -> int32_t
 {
-  std::signal(SIGINT, handle_end);
+  std::signal(SIGINT, handle_process_finalization);
   std::vector<std::thread> threads_to_process_emails{};
 
-  threads_to_process_emails.push_back(std::thread(process_queue, "1", "iso45823@xcoxc.com"));
-  // threads_to_process_emails.push_back(std::thread(process_queue, "2", "fhm70240@xcoxc.com"));
+  threads_to_process_emails.push_back(std::thread(process_single_queue, "R2", "iso45823@xcoxc.com"));
+  // threads_to_process_emails.push_back(std::thread(process_single_queue, "R1", "fhm70240@xcoxc.com"));
 
   if (threads_to_process_emails[0].joinable())
     threads_to_process_emails[0].join();
